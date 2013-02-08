f41f107d65fb2adf4918986962950365631a8a62
ryanffvc <ryan@ffvc.com>
2013-02-08 08:43:00 -0500

    initial commit

---
+++ parties/admin.py
@@ -0,0 +1,38 @@
+from parties.models import *
+from django.contrib import admin
+from django.contrib.auth.models import User
+
+class CocktailIngredientInline(admin.TabularInline):
+    model = CocktailIngredient
+    extra = 1
+
+class CocktailAdmin(admin.ModelAdmin):
+    inlines = (CocktailIngredientInline,)
+
+class PartyCocktailInline(admin.TabularInline):
+    model = PartyCocktail
+    extra = 1
+
+class PartyAdmin(admin.ModelAdmin):
+    inlines = (PartyCocktailInline,)
+
+class InvitationInline(admin.TabularInline):
+    model = Invitation
+    extra = 1
+
+class GuestlistAdmin(admin.ModelAdmin):
+    inlines = (InvitationInline,)
+
+admin.site.register(Process)
+admin.site.register(Preparation)
+admin.site.register(Category)
+admin.site.register(Ingredient)
+admin.site.register(Cocktail, CocktailAdmin)
+admin.site.register(Style)
+admin.site.register(Party, PartyAdmin)
+admin.site.register(Guest)
+admin.site.register(Guestlist, GuestlistAdmin)
+
+
+
+
---
+++ parties/models.py
@@ -0,0 +1,139 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Preparation(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+    
+    def __unicode__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name_plural = "preparations"
+        ordering = ('name',)
+
+class Process(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+    
+    def __unicode__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name_plural = "processes"
+        ordering = ('name',)
+
+class Category(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+    
+    def __unicode__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name_plural = "categories"
+        ordering = ('name',)
+
+class Style(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+    
+    def __unicode__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name_plural = "styles"
+        ordering = ('name',)
+
+class CocktailIngredient(models.Model):
+    cocktail = models.ForeignKey('Cocktail', related_name='cocktail_ingredients')
+    ingredient = models.ForeignKey('Ingredient')
+    amount = models.IntegerField(default=1)
+    order = models.IntegerField(default=1,
+        help_text="Controls the order in which ingredients are displayed.")
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=48, unique=True)    
+    preparation = models.ForeignKey('Preparation', null=True, blank=True)
+    process = models.ForeignKey('Process', null=True, blank=True)  
+    categories = models.ForeignKey(Category, null=True, blank=True)
+    
+    def __unicode__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name_plural = "ingredients"
+        ordering = ('name',)
+
+class Cocktail(models.Model):
+    name = models.CharField(max_length=48, unique=True)
+    ingredients = models.ManyToManyField('Ingredient', related_name='cocktail_ingredients', blank=True,
+        through=CocktailIngredient)
+
+    def __unicode__(self):
+        return self.name
+    
+    class Meta:
+        ordering = ('name',)
+
+class ListedCocktail(models.Model):
+    cocktaillist = models.ForeignKey('Cocktaillist', related_name='listed_cocktail')
+    cocktail = models.ForeignKey('Cocktail')
+
+class Cocktaillist(models.Model):
+    name = models.CharField(max_length=48, unique=True)    
+    cocktail = models.ManyToManyField('Cocktail', related_name='listed_cocktail', blank=True,
+            through=ListedCocktail)
+
+    def __unicode__(self):
+        return self.name
+    
+    class Meta:
+        ordering = ('name',)
+
+class Guest(models.Model):
+#    user = models.OneToOneField(User)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField(max_length=254)
+   
+    def __unicode__(self):
+        return self.full_name
+    
+    class Meta:
+        verbose_name_plural = "people"
+        ordering = ('last_name', 'first_name')
+    
+    @property
+    def full_name(self):
+        return '%s %s' % (
+            self.first_name,
+            self.last_name,
+        )
+
+class Invitation(models.Model):
+    guestlist = models.ForeignKey('Guestlist', related_name='invited_guests')
+    guest = models.ForeignKey('Guest')
+
+class Guestlist(models.Model):
+    name = models.CharField(max_length=48, unique=True)    
+    guest = models.ManyToManyField('Guest', related_name='invited_guests', blank=True,
+            through=Invitation)
+
+    def __unicode__(self):
+        return self.name
+    
+    class Meta:
+        ordering = ('name',)
+
+class Party(models.Model):
+    name = models.CharField(max_length=48, unique=True)
+    cocktaillist = models.ManyToManyField('Cocktaillist', blank=True,)
+    guestlist = models.ForeignKey('Guestlist', blank=True,)
+
+    def __unicode__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name_plural = "parties"
+        ordering = ('name',)
+
+
+
