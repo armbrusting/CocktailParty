149a5b73b1ebbdb2f5af735808aa6c4bd1f93f34
ryanffvc <ryan@ffvc.com>
2013-02-08 17:56:07 -0500

    and through a couple views

--- Cocktail_Party/urls.py
+++ Cocktail_Party/urls.py
@@ -4,14 +4,10 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^parties/', include('parties.urls')),
-    # Examples:
-    # url(r'^$', 'Cocktail_Party.views.home', name='home'),
-    # url(r'^Cocktail_Party/', include('Cocktail_Party.foo.urls')),
+    url(r'^parties/', include('parties.urls', namespace="parties")),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 )
--- parties/views.py
+++ parties/views.py
@@ -1,18 +1,17 @@
 from django.http import HttpResponse
 from django.template import Context, loader
+from django.shortcuts import render, get_object_or_404
 
 from parties.models import Party
 
 def index(request):
-    latest_party_list = Party.objects.all
-    template = loader.get_template('index.html')
-    context = Context({
-        'latest_party_list': latest_party_list,
-    })
-    return HttpResponse(template.render(context))
+    latest_party_list = Party.objects.all()
+    context = {'latest_party_list': latest_party_list}
+    return render(request, 'party/index.html', context)
 
 def detail(request, party_id):
-    return HttpResponse("You're looking at party %s." % party_id)
+    party = get_object_or_404(Party, pk=party_id)
+    return render(request, 'party/detail.html', {'party': party})
 
 def cocktails(request, party_id):
     return HttpResponse("You're looking at the cocktails of party %s." %
